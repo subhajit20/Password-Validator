a2d9b6d32a04e874a930eecd7f19f044f25369bb
Subhajit Ghosh <99127578+subhajit20@users.noreply.github.com>
2022-10-27 16:09:33 +0000

    Made password validator function

--- Password_Validator.py
+++ Password_Validator.py
@@ -1,16 +1,28 @@
 # Password validator
-
 import re
+
+
+def Custom_Password_Validator(password):
+  try:
+    expression = r'([A-Z]+\B)([a-z]+\B)([0-9]+)(\W+)'
+
+    flag = re.search(expression, password)
+
+    if flag is not None:
+      return flag.group()
+    else:
+      return False
+
+  except Exception as e:
+    print(e)
+
 
 password = input("Enter your password --> ")
 
-expression = r'([A-Z]+\B)([a-z]+\B)([0-9]+)(\W+)'
-
-flag = re.search(expression, password)
-
-if flag is not None:
-  print(flag)
+data = Custom_Password_Validator(password)
+if data != False:
+  print(data)
 else:
   print(
-    "Your password should be 8 characters long, with Uppercase , strats with upper case, lowercase, digit and special characters"
+    "Your password should be 8 characters long, Starts with uppercase, then lowercase , then digits and then special characters except _"
   )
---
+++ README.md
@@ -0,0 +1,4 @@
+This is a custom password validator function which validate your password whether it is 8 characters long or not.
+
+> In a nutshell, the custom password validation function take a password as arguement and then checks some regular expression validation
+
